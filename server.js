b69b218f854b8561aa3063fa2414753d2901c212
Hewan Redie <tewol22@gmail.com>
2021-07-15 00:47:37 -0500

    more updates

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
@@ -8,11 +9,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-
-//require('./routes/apiRoutes')(app);
-//require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
    
 app.listen(PORT, function() {
